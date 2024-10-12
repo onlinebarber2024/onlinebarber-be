c2fc20d666cb0ddc6751b4794b5e7086778319b5
algoritmamaratonu <canfurkan903@gmail.com>
2024-10-12 10:07:55 +0300

    first commit

---
+++ onlinebarber/src/main/java/com/rf/onlinebarber/OnlinebarberApplication.java
@@ -0,0 +1,13 @@
+package com.rf.onlinebarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebarberApplication.class, args);
+	}
+
+}
---
+++ onlinebarber/src/test/java/com/rf/onlinebarber/OnlinebarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rf.onlinebarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
